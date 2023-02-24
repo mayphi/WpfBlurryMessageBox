f9a8b97d9734ed071bf00e4b84f74cb137ab61ef
Mayr Philipp <philipp.mayr@sigmatek.at>
2023-02-24 06:22:04 +0100

    Added Project

---
+++ BlurryMessageBox/BlurryMessageBox/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace BlurryMessageBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show( "MyMessageBoxText", "MyCaption", MessageBoxButton.YesNoCancel, MessageBoxImage.Warning);
+        }
+    }
+}
